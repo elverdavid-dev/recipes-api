3b85116f90ea0c2036f0e2803b32f5a30b5b8f14
elverdavid <elverdavid0839@gmail.com>
2023-09-16 16:45:25 -0500

    servicio de countrys

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { CloudinaryConfigProvider } from './utils/cloudinary.config';
+import { CountrysModule } from './countrys/countrys.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CloudinaryConfigProvider } from './utils/cloudinary.config';
     CacheModule.register({ isGlobal: true, ttl: 60000, store: 'memory' }),
     RecipesModule,
     CategoriesModule,
+    CountrysModule,
   ],
   controllers: [],
   providers: [CloudinaryConfigProvider],
