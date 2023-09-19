77699c2fa875748f693bd9188ccdfe8af6fbcab9
elverdavid <elverdavid0839@gmail.com>
2023-09-18 21:21:46 -0500

    crud de el servicio de country creado

--- src/countrys/countrys.service.ts
+++ src/countrys/countrys.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCountryDto } from './dto/create-country.dto';
 import { UpdateCountryDto } from './dto/update-country.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Country } from './entities/country.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class CountrysService {
-  create(createCountryDto: CreateCountryDto) {
-    return 'This action adds a new country';
+  constructor(
+    @InjectModel(Country.name) private CountryEntity: Model<Country>,
+  ) {}
+
+  async create(createCountryDto: CreateCountryDto) {
+    const newCountry = await this.CountryEntity.create(createCountryDto);
+    return newCountry.save();
   }
 
-  findAll() {
-    return `This action returns all countrys`;
+  async findAll() {
+    return await this.CountryEntity.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} country`;
+  async findOne(id: number) {
+    return await this.CountryEntity.findById(id);
   }
 
-  update(id: number, updateCountryDto: UpdateCountryDto) {
-    return `This action updates a #${id} country`;
+  async update(id: number, updateCountryDto: UpdateCountryDto) {
+    return this.CountryEntity.findByIdAndUpdate(id, updateCountryDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} country`;
+  async remove(id: number) {
+    return await this.CountryEntity.findByIdAndDelete(id);
   }
 }
