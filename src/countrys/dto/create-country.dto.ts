c5234b1156b11d67249dfd49d01d0bd3b7f8cb39
elverdavid <elverdavid0839@gmail.com>
2023-09-18 21:13:58 -0500

    dto de el servicio de country , agregados

--- src/countrys/dto/create-country.dto.ts
+++ src/countrys/dto/create-country.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCountryDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCountryDto {
+  @IsString({ message: 'El nombre tiene que ser de tipo string!' })
+  @IsNotEmpty({ message: 'El nombre es requerido!' })
+  name: string;
+  image: string;
+}
--- src/countrys/dto/update-country.dto.ts
+++ src/countrys/dto/update-country.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCountryDto } from './create-country.dto';
+import { IsString } from 'class-validator';
 
-export class UpdateCountryDto extends PartialType(CreateCountryDto) {}
+export class UpdateCountryDto extends PartialType(CreateCountryDto) {
+  @IsString({ message: 'El nombre tiene que ser de tipo string!' })
+  name?: string;
+
+  image?: string;
+
+  public_id?: string;
+}
