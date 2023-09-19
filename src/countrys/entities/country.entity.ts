6ca3abbb48d985b0ad9f5e90f5af09feadb8e4ee
elverdavid <elverdavid0839@gmail.com>
2023-09-18 20:42:57 -0500

    entidad de regiones agregada

--- src/countrys/entities/country.entity.ts
+++ src/countrys/entities/country.entity.ts
@@ -1,1 +1,15 @@
-export class Country {}
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+
+@Schema({ versionKey: false })
+export class Country {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  image: string;
+
+  @Prop()
+  public_id: string;
+}
+
+export const CountryEntity = SchemaFactory.createForClass(Country);
