d091f144beacfee373a3dbae2441fd8ac13911b6
elverdavid <elverdavid0839@gmail.com>
2023-09-22 20:37:23 -0500

    pequeños ajustes a el sistema de paginacion

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -40,7 +40,7 @@ export class RecipesService {
     //Obtener el total de recetas agregadas a la DB
     const totalRecipes = await this.RecipeEntity.countDocuments();
 
-    const { currentPage, totalItems, totalPages } = paginateResults(
+    const { currentPage, totalItems, totalPages, skip } = paginateResults(
       totalRecipes,
       page,
       limit,
@@ -52,7 +52,6 @@ export class RecipesService {
     }
 
     //Si no hay datos en cache entoce se agregan
-    const skip = (page - 1) * limit;
     const listRecipes = await this.RecipeEntity.find()
       .skip(skip)
       .limit(limit)
@@ -64,7 +63,7 @@ export class RecipesService {
     const recipePageData = {
       page: currentPage,
       totalPages,
-      totalRecipes: totalItems,
+      totalItems,
       data: listRecipes,
     };
     await this.cacheManager.set(this.cacheKey, recipePageData);
@@ -160,7 +159,7 @@ export class RecipesService {
     const pageData = {
       page: currentPage,
       totalPages,
-      totalCountrys: totalItems,
+      totalItems,
       data: recipes,
     };
     return pageData;
